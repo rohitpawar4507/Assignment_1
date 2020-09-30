721a83378a57d3604aaa6b48f8c0c51082d3626d
rohitpawar4507 <67743293+rohitpawar4507@users.noreply.github.com>
2020-09-30 17:39:10 +0530

    Add files via upload

---
+++ SdL_1.py
@@ -0,0 +1,24 @@
+# Assignment No.1
+# Write Python code for Word count and find occurrence of word from file.
+
+fo=open("sdl.txt",'r')
+str=fo.read()
+data=str.split()
+counts=dict()
+
+for i in data:
+    if i in counts:
+        counts[i]+=1
+    else:
+            counts[i]=1
+
+w=len(str)
+l=len(data)
+
+print("Total Characters in the file is :",w)
+print("Total Words in the file is :",l)
+print("occurrence:",counts)
+
+print("thank you 😊!")
+
+fo.close()
